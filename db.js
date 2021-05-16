ee669822e48f61d5c0335c4d6a790fee4a1862cd
yazanmo <yazanmostafa96@gmail.com>
2021-05-16 12:57:29 +0300

    pulse check 2 done

--- db.js
+++ db.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+
+const option = {
+
+    useCreaeIndex:true
+    useFindAndModify:true,
+    useUnifiedTopology:true,
+    useNewUrlparser:true,
+
+}
+
+
+mongoose.connect("mongodb://localhost:27017",option).then(
+    ()=>{ console.log ("DB connected"),
+    (err)=>{console.log(err)}}
+)
